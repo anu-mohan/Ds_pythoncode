cf50703cb4c94f23aac9769904f953b8f1d5c3f8
anu-mohan <80379775+anu-mohan@users.noreply.github.com>
2021-03-17 13:19:47 +0530

    Add files via upload

---
+++ 17marclass2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#list
+#ordered collection of item \elements
+#syntax is []
+languages=["python","c","c++","java"]
+
+
+# In[1]:
+
+
+word = "aeioubcdfg"
+print(word[:3] + word[3:])
+
+
+# In[8]:
+
+
+s='Hello, "How are you" '
+
+
+# In[9]:
+
+
+print(s)
+
